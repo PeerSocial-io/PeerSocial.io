3cf29b49c61d9c366ec4fd75be465340545a491e
Bradley Matusiak <bmatusiak@gmail.com>
2022-05-31 18:49:24 +0000

    update

--- build_gun.sh
+++ build_gun.sh
@@ -1,2 +1,5 @@
+rm -rf ./node_modules/gun
+npm install gun
 cd ./node_modules/gun
-npm run unbuild && npm run unbuildSea
+node lib/unbuild
+node lib/unbuild sea
--- run-build.sh
+++ run-build.sh
@@ -1,5 +1,6 @@
 # git rev-parse --verify HEAD >> GIT_HEAD
-echo Build $NODE_ENV
+echo Build $APP_ENV
+# npm install
 # env
 sh ./build_gun.sh
 npm run build-app
