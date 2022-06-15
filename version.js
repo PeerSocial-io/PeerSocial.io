5291ed4f6396a11e1475924b224ec9b8e5fa0f3d
Bradley Matusiak <bmatusiak@gmail.com>
2022-06-15 11:25:43 +0000

    update

--- version.js
+++ version.js
@@ -3,8 +3,8 @@ var $version = $package.version.split(".");
 var date = new Date();
 
 var year = date.getFullYear();
-var month = date.getMinutes();
-var day = date.getDay();
+var month = date.getMonth()+1;
+var day = date.getDate();
 var hour = date.getHours()+"";
 var min = date.getMinutes();
 var sec = date.getSeconds();
@@ -12,6 +12,5 @@ var sec = date.getSeconds();
 var rc = hour+""+min+""+sec
 
 $version[1] = year;
-$version[2] = month+""+day;
-$version[2] += "-"+rc;
+$version[2] = ""+month+""+day+"-"+rc;
 console.log($version.join("."))
