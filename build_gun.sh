1dfb51691f30e3a380301a16059a9d8023b0a46e
Bradley Matusiak <bmatusiak@gmail.com>
2022-07-09 13:00:55 +0000

    fixed paths

--- build_gun.sh
+++ build_gun.sh
@@ -1,6 +1,9 @@
 # rm -rf ./node_modules/gun
 # npm install gun
-ln -sf ./node_modules/gun ./gun
+GUN_PATH=$(node -e "console.log(require('path').dirname(require.resolve('gun')))") 
+# PSIO_CLI_PATH=$(node -e "console.log(require('path').resolve(__dirname))") 
+
+ln -sf $GUN_PATH ./gun
 cd ./gun
 node lib/unbuild
 node lib/unbuild sea
